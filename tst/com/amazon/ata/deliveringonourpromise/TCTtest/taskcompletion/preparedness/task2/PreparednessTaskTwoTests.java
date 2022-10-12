6160d8436fe2c6522a0ef121bff92ab8b11bf671
SZ-Don <don.platan@talentamp.com>
2022-10-12 15:12:51 +0800

    Created OrderDao test plan.

---
+++ tst/com/amazon/ata/deliveringonourpromise/TCTtest/taskcompletion/preparedness/task2/PreparednessTaskTwoTests.java
@@ -0,0 +1,11 @@
+package com.amazon.ata.deliveringonourpromise.TCTtest.taskcompletion.preparedness.task2;
+
+import org.junit.jupiter.api.Test;
+
+public class PreparednessTaskTwoTests {
+    @Test
+    public void preparednessTaskTwo_preparednessTaskTwoFile_existsAndItsNotEmpty() {
+
+    }
+
+}
--- tst/com/amazon/ata/deliveringonourpromise/dao/OrderDaoTest.java
+++ tst/com/amazon/ata/deliveringonourpromise/dao/OrderDaoTest.java
@@ -1,7 +1,27 @@
 package com.amazon.ata.deliveringonourpromise.dao;
 
+import com.amazon.ata.deliveringonourpromise.App;
+import com.amazon.ata.deliveringonourpromise.deliverypromiseservice.DeliveryPromiseServiceClient;
+import com.amazon.ata.deliveringonourpromise.ordermanipulationauthority.OrderManipulationAuthorityClient;
+import com.amazon.ata.deliveringonourpromise.types.Order;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class OrderDaoTest {
 
     private OrderDao dao;
+    private OrderManipulationAuthorityClient omaClient = App.getOrderManipulationAuthorityClient();
 
+    @Test
+    public void get_forKnownOrderId_returnsOrder() {
+        // GIVEN
+        String orderId = "111-7497023-2960775";
+        dao = new OrderDao(omaClient);
+
+        // WHEN
+        Order result = dao.get(orderId);
+
+        // THEN
+        Assertions.assertNotEquals(null, result, "PASSED");
+    }
 }
